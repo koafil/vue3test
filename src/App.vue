5301f456ef619d9a9e80a3073c235cc335940d6e
kfil <koafil@gmail.com>
2022-05-25 16:27:43 +0300

    Вывод и создание постов

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,96 @@
 <template>
-<div>
-  Hi!   Variant xz
+<div class="app">
+  <form @submit.prevent>
+    <h4>Создание поста</h4>
+    <input 
+      v-bind:value="title" 
+      @input="inputTitle"
+      class="input" 
+      type="text" 
+      placeholder="Название"
+    >
+    <input 
+    v-bind:value="body" 
+    @input="inputBody"
+    class="input" 
+    type="text" 
+    placeholder="Описание"
+    >
+    <button class="btn" @click="createPost">Создать</button>
+  </form>
+ <div class = 'post' v-for = 'post in posts' >
+  <div><strong>Название:</strong> {{ post.title }} </div>
+  <div><strong>Описание:</strong> {{ post.body }} </div>
+ </div>
 </div>
+
 </template>
 
+<script>
+export default {
+  data() {
+    return {
+      posts: [
+        {id: 1, title: 'JavaScript', body: 'Описание поста 1'},
+        {id: 2, title: 'JavaScript 2', body: 'Описание поста 2'},
+        {id: 3, title: 'JavaScript 3', body: 'Описание поста 3'},
+        {id: 4, title: 'JavaScript 4', body: 'Описание поста 4'},
+      ],
+      title: '',
+      body:''
+    }
+  },
+  methods: {
+    createPost() {
+      const newPost = {
+        id: Date.now(),
+        title: this.title,
+        body: this.body
+      }
+      this.posts.push(newPost);
+      this.title="";
+      this.body="";
+    },
+    inputTitle(event) {
+      this.title = event.target.value;
+    },
+    inputBody(event) {
+      this.body = event.target.value;
+    },
+  }
+}
+</script>
+
 <style>
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+.app {
+  padding: 20px;
+}
+.post {
+  padding: 15px;
+  border: 2px solid teal;
+  margin-top: 15px;
+}
+form {
+  display: flex;
+  flex-direction: column;
+}
+.input {
+  width: 100%;
+  border: 1px solid teal;
+  padding: 10px 15px;
+  margin-top: 15px;
+}
+.btn {
+  margin-top: 15px;
+  align-self: flex-end;
+  padding: 10px 15px;
+  background: none;
+  color: teal;
+  border: 1px solid teal;
+}
 </style>
