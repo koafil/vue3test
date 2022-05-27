a72f798e7af66f72cd61ad21168f7c78c66eb1c9
kfil <koafil@gmail.com>
2022-05-27 22:43:31 +0300

    Сортировка постов  (computed)

--- src/App.vue
+++ src/App.vue
@@ -18,7 +18,7 @@
       />
     </my-dialog>
     <post-list 
-      :posts="posts"
+      :posts="sortedPosts"
       @remove = "removePost"
       v-if = "!isPostsLoading"
     />
@@ -73,12 +73,12 @@ export default {
   mounted() {
     this.fetchPosts();
   },
+  computed: {
+    sortedPosts() {
+      return [...this.posts].sort((post1, post2)=>post1[this.selectedSort]?.localeCompare(post2[this.selectedSort]));
+    }
+  },
   watch: {
-    selectedSort(newValue){
-      this.posts.sort((post1, post2)=>{
-        return post1[newValue]?.localeCompare(post2[newValue]);
-      });
-    },
   }
 }
 </script>
