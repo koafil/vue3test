ae1c998c162cc8f452b65ba3e72f444e04f46945
kfil <koafil@gmail.com>
2022-05-27 23:22:39 +0300

    Добавлена строка поиска

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,18 @@
 <template>
   <div class="app">
     <h1>Страница с постами</h1>
+    <my-input v-model="searchQuery" placeholder="Поиск..." />
     <div class="app__btns">
-      <my-button
-        @click = "showDialog" 
-      >
-        Создать пост
-      </my-button>
-      <my-select 
-        v-model="selectedSort"
-        :options="sortOptions"
-      />
+      <my-button @click="showDialog"> Создать пост </my-button>
+      <my-select v-model="selectedSort" :options="sortOptions" />
     </div>
     <my-dialog v-model:show="dialogVisible">
-      <post-form 
-        @create = "createPost"
-      />
+      <post-form @create="createPost" />
     </my-dialog>
-    <post-list 
-      :posts="sortedPosts"
-      @remove = "removePost"
-      v-if = "!isPostsLoading"
+    <post-list
+      :posts="sortedAndSearchedPosts"
+      @remove="removePost"
+      v-if="!isPostsLoading"
     />
     <div v-else>Идет загрузка...</div>
     <!-- <post-list /> -->
@@ -33,20 +25,22 @@ import PostForm from "./components/PostForm";
 
 export default {
   components: {
-    PostList, PostForm
+    PostList,
+    PostForm,
   },
   data() {
     return {
       posts: [],
       dialogVisible: false,
       isPostsLoading: true,
-      selectedSort: '',
+      selectedSort: "",
+      searchQuery: "",
       sortOptions: [
-        {value: 'title', name: 'По названию'},
-        {value: 'body', name: 'По содержимому'},
-        {value: 'id', name: 'По идентификатору'},
-      ]
-    }
+        { value: "title", name: "По названию" },
+        { value: "body", name: "По содержимому" },
+        { value: "id", name: "По идентификатору" },
+      ],
+    };
   },
   methods: {
     createPost(post) {
@@ -54,36 +48,42 @@ export default {
       this.dialogVisible = false;
     },
     removePost(post) {
-      this.posts=this.posts.filter(p=>p.id != post.id);
+      this.posts = this.posts.filter((p) => p.id != post.id);
     },
-    showDialog()
-    {
+    showDialog() {
       this.dialogVisible = true;
     },
     fetchPosts() {
-      setTimeout(()=>{
-      this.isPostsLoading=true;
-      fetch('https://jsonplaceholder.typicode.com/posts?_limit=10')
-        .then(response => response.json())
-        .then(json => this.posts=json)
-        .catch(()=>alert("Error"))
-        .finally(()=>this.isPostsLoading=false);
-      },1000);
-    }
+      setTimeout(() => {
+        this.isPostsLoading = true;
+        fetch("https://jsonplaceholder.typicode.com/posts?_limit=10")
+          .then((response) => response.json())
+          .then((json) => (this.posts = json))
+          .catch(() => alert("Error"))
+          .finally(() => (this.isPostsLoading = false));
+      }, 1000);
+    },
   },
   mounted() {
     this.fetchPosts();
   },
   computed: {
     sortedPosts() {
-      if(this.selectedSort === 'id')
-      return [...this.posts].sort((post1, post2)=>post1.id>post2.id);
-      else return [...this.posts].sort((post1, post2)=>post1[this.selectedSort]?.localeCompare(post2[this.selectedSort]));
-    }
+      if (this.selectedSort === "id")
+        return [...this.posts].sort((post1, post2) => post1.id > post2.id);
+      else
+        return [...this.posts].sort((post1, post2) =>
+          post1[this.selectedSort]?.localeCompare(post2[this.selectedSort])
+        );
+    },
+    sortedAndSearchedPosts() {
+      return this.sortedPosts.filter((post) =>
+        post.title.includes(this.searchQuery)
+      );
+    },
   },
-  watch: {
-  }
-}
+  watch: {},
+};
 </script>
 
 <style>
@@ -99,6 +99,5 @@ export default {
   display: flex;
   justify-content: space-between;
   margin: 15px 0;
-
 }
 </style>
